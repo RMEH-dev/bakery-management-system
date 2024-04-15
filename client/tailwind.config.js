d899618b3848aba9cc373bea5019ad9fb670db04
Eranda De Silva <desilva-im20008@stu.kln.ac.lk>
2024-04-15 20:56:16 +0530

    navbar initialize

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,8 +5,15 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "1600px",
+      },
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      montserrat: ["Montserrat", "sans-serif"],
+    },
   },
   plugins: [],
 });
-
