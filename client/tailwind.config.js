9f1a3aaaed1becea6b921528fd05e029866be3b0
Eranda De Silva <desilva-im20008@stu.kln.ac.lk>
2024-04-15 20:49:14 +0530

    tailwind and material-tailwind configuration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+});
 
