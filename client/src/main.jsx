333c024aefeae94ffc24b42a396c19a7e9edbe82
Eranda De Silva <desilva-im20008@stu.kln.ac.lk>
2024-04-17 21:37:55 +0530

    created products.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
 import Home from "./pages/customer/home.jsx";
+import Products from "./pages/customer/products.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/products",
+    element: <Products/>,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <div className="bg-brown-400">
-        <Home />
-      </div>
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );
