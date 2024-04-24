401c61abce12c2447271cf7987f7bafd0c1db924
Eranda De Silva <desilva-im20008@stu.kln.ac.lk>
2024-04-25 02:00:29 +0530

    server config
    installation of packages

--- server/server.js
+++ server/server.js
@@ -1,6 +1,30 @@
-const express = require('express')
+const express = require('express');
+const path = require('path'); 
+const mysql = require('mysql');
+const dotenv = require('dotenv');
 
-const app = express()
+dotenv.config({path: './.env'});
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE
+});
+
+
+
+app.set('view engine')
+
+db.connect((error) => {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log('Database Connected...')
+  }
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
