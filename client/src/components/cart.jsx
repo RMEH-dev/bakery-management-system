14908ac11f1a28387b23c5442245dc1856664cff
Eranda De Silva <desilva-im20008@stu.kln.ac.lk>
2024-05-11 22:06:43 +0530

    cart design complete

--- client/src/components/cart.jsx
+++ client/src/components/cart.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { MegaMenuWithHover } from "./navbar";
 import { FooterWithSocialLinks } from "./footer";
 import {
@@ -7,22 +7,42 @@ import {
   Checkbox,
   Button,
   Typography,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
 } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
+import { CheckIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
 import QuantityBtn from "./quantitybtn";
 
 const TABLE_HEAD = ["Product", "Price", "Quantity", "SubTotal"];
- 
+
 const TABLE_ROWS = [
   {
     Product: "",
     Price: "",
     Quantity: "",
-    SubTotal:"",
+    SubTotal: "",
   },
 ];
 
 export function Cart() {
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedOption2, setSelectedOption2] = useState(null);
+  const [isDropdownOpen2, setIsDropdownOpen2] = useState(false);
+
+  const handleSelect = (option) => {
+    setSelectedOption(option);
+    setIsDropdownOpen(false);
+  };
+
+  const handleSelect2 = (option) => {
+    setSelectedOption2(option);
+    setIsDropdownOpen2(false);
+  };
+
   const products = [1, 2, 3, 4];
   return (
     <div>
@@ -68,98 +88,167 @@ export function Cart() {
               </tr>
             </thead>
             <tbody>
-              {TABLE_ROWS.map(({ Product, Price, Quantity, SubTotal }, index) => {
-                const isLast = index === TABLE_ROWS.length - 1;
-                const classes = isLast
-                  ? "p-4"
-                  : "p-4 border-b border-blue-gray-50";
+              {TABLE_ROWS.map(
+                ({ Product, Price, Quantity, SubTotal }, index) => {
+                  const isLast = index === TABLE_ROWS.length - 1;
+                  const classes = isLast
+                    ? "p-4"
+                    : "p-4 border-b border-blue-gray-50";
 
-                return (
-                  <tr key={Product}>
-                    <td className={classes}>
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal"
-                      >
-                        {Product}
-                      </Typography>
-                    </td>
-                    <td className={classes}>
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal"
-                      >
-                        {Price}
-                      </Typography>
-                    </td>
-                    <td className={classes}>
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal pl-16 justify-center items-center"
-                      >
-                         <QuantityBtn/>
-
-                      </Typography>
-                    </td>
-                    <td className={classes}>
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal"
-                      >
-                        {SubTotal}
-                      </Typography>
-                    </td>
-                  </tr>
-                );
-              })}
+                  return (
+                    <tr key={Product}>
+                      <td className={classes}>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal"
+                        >
+                          {Product}
+                        </Typography>
+                      </td>
+                      <td className={classes}>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal"
+                        >
+                          {Price}
+                        </Typography>
+                      </td>
+                      <td className={classes}>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal pl-16 justify-center items-center"
+                        >
+                          <QuantityBtn />
+                        </Typography>
+                      </td>
+                      <td className={classes}>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal"
+                        >
+                          {SubTotal}
+                        </Typography>
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
             </tbody>
           </table>
         </Card>
       </div>
-      <div className="z-100 -mt-[650px] ml-[850px] h-[750px] w-[300px] md:w-[400px] lg:w-[300px] xl:w-[650px] 2xl:w-[800px] mb-6 rounded-2xl bg-c4 text-c3 hover:text-c1 flex flex-col space-y-1">
+      <div className="z-100 -mt-[650px] ml-[850px] h-[750px] w-[300px] md:w-[400px] lg:w-[300px] xl:w-[650px] 2xl:w-[650px] mb-6 rounded-2xl bg-c4 text-c3 hover:text-c1 flex flex-col space-y-1">
         <Card
-          className="flex flex-col h-[750px] sm:w-auto bg-gradient-to-bl from-white to-c4 rounded-2xl z-80"
+          className="flex flex-col justify-items-center h-[750px] sm:w-auto bg-gradient-to-bl from-c5 to-c2 rounded-2xl z-80"
           shadow={false}
         >
           <form className="ml-[50px] mt-5 mb-2 w-[300px] 2xl:w-[800px] h-150 max-w-screen-lg sm:w-96">
-            <div className="mb-1 flex flex-col gap-6">
+            <div className="mb-2 flex flex-col gap-6">
               <Typography className="-mb-3 text-xl text-black font-bold font-[Montserrat]">
                 Cart Details
               </Typography>
-              <Typography className="-mb-3 text-black font-semibold font-[Montserrat]">
-                Sub Total
-              </Typography>
-              <Input
-                type="text"
-                size="md"
-                placeholder="lastName"
-                className="-mb-3 w-[320px] text-black font-semibold font-[Montserrat] border-deep-orange-200 focus:!border-deep-orange-900 bg-c1 rounded-[30px]"
-                labelProps={{
-                  className: "before:content-none after:content-none",
-                }}
-              />
+              <div className="mt-3 grid flex grid-cols-2 gap-5">
+                <Typography className="mb-3 pl-2 w-[520px] bg-c4 rounded-2xl text-black font-semibold text-lg font-[Montserrat]">
+                  Sub Total
+                </Typography>
+                <Typography className="mb-3  text-black font-semibold text-lg font-[Montserrat]">
+                  Rs.5500.00
+                </Typography>
               </div>
-              <Typography className="-mb-3 text-black font-semibold font-[Montserrat]">
-                User Name
-              </Typography>
-              <Input
-                type="text"
-                size="md"
-                placeholder="userName"
-                className="-mb-3 w-[320px] text-black font-semibold font-[Montserrat] border-deep-orange-200 focus:!border-deep-orange-900 bg-c4 rounded-[30px]"
-                labelProps={{
-                  className: "before:content-none after:content-none",
-                }}
-              />
-              <Button
-                className="mt-4 w-[500px] hover:bg-deep-orange-900 bg-c3 rounded-3xl text-white text-xl font-[Montserrat]"
-              >
-                Save Changes
-              </Button>
+              <div className="-mt-2 grid inline-block flex grid-cols-2 gap-5">
+                <Typography
+                  className={`mb-3 pt-2 cursor-pointer pl-2 w-[520px] bg-c4 rounded-2xl text-black font-semibold text-lg font-[Montserrat] `}
+                  onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                >
+                  Pickup or Delivery
+                  <ChevronDownIcon className="ml-44 -mt-6 w-5 h-5" />
+                  {isDropdownOpen && (
+                    <ul className="mt-3 mb-3 cursor-pointer rounded-2xl text-c3 w-[300px] text-lg font-bold font-[Montserrat] bg-c2">
+                      <li
+                        onClick={() => handleSelect("Pickup")}
+                        className={
+                          selectedOption === "Pickup"
+                            ? "bg-c5 flex rounded-2xl justify-between items-center p-4"
+                            : "flex justify-between items-center p-4"
+                        }
+                      >
+                        Pickup
+                        {selectedOption === "Pickup" && (
+                          <CheckIcon className="w-5 h-5 text-green-500" />
+                        )}
+                      </li>
+                      <li
+                        onClick={() => handleSelect("Delivery")}
+                        className={
+                          selectedOption === "Delivery"
+                            ? "bg-c5 flex rounded-2xl justify-between items-center p-4"
+                            : "flex justify-between items-center p-4"
+                        }
+                      >
+                        Delivery
+                        {selectedOption === "Delivery" && (
+                          <CheckIcon className="w-5 h-5 text-green-500" />
+                        )}
+                      </li>
+                    </ul>
+                  )}
+                </Typography>
+              </div>
+              <div className="-mt-2 grid flex grid-cols-2 gap-5">
+                <Typography className="mb-3 pl-2 w-[520px] bg-c4 rounded-2xl text-black font-semibold text-lg font-[Montserrat]">
+                  Total
+                </Typography>
+                <Typography className="mb-3  text-black font-semibold text-lg font-[Montserrat]">
+                  Rs.5500.00
+                </Typography>
+              </div>
+              <div className="-mt-2 grid inline-block flex grid-cols-2 gap-5">
+                <Typography
+                  className={`mb-3 pt-2 cursor-pointer pl-2 w-[520px] bg-c4 rounded-2xl text-black font-semibold text-lg font-[Montserrat] `}
+                  onClick={() => setIsDropdownOpen2(!isDropdownOpen2)}
+                >
+                  Discount Type
+                  <ChevronDownIcon className="ml-36 -mt-6 w-5 h-5" />
+                  {isDropdownOpen2 && (
+                    <ul className="mt-3 mb-3 cursor-pointer rounded-2xl text-c3 w-[500px] text-lg font-bold font-[Montserrat] bg-c2">
+                      <li
+                        onClick={() => handleSelect2("Coupon: Enter Coupon at the Checkout")}
+                        className={
+                          selectedOption2 === "Coupon: Enter Coupon at the Checkout"
+                            ? "bg-c5 flex rounded-2xl justify-between items-center p-4"
+                            : "flex justify-between items-center p-4"
+                        }
+                      >
+                        Coupon: Enter Coupon at the Checkout 
+                        {selectedOption2 === "Coupon: Enter Coupon at the Checkout" && (
+                          <CheckIcon className="w-5 h-5 text-green-500" />
+                        )}
+                      </li>
+                      <li
+                        onClick={() => handleSelect2("Special Discount: Specify at the Checkout")}
+                        className={
+                          selectedOption2 === "Special Discount: Specify at the Checkout"
+                            ? "bg-c5 flex rounded-2xl justify-between items-center p-4"
+                            : "flex justify-between items-center p-4"
+                        }
+                      >
+                        Special Discount: Specify at the Checkout
+                        {selectedOption2 === "Special Discount: Specify at the Checkout" && (
+                          <CheckIcon className="w-5 h-5 text-green-500" />
+                        )}
+                      </li>
+                    </ul>
+                  )}
+                </Typography>
+              </div>
+            </div>
+            <Button className="mt-4 w-[520px] hover:bg-deep-orange-900 bg-c3 rounded-3xl text-white text-xl font-[Montserrat]">
+              Proceed to Checkout
+            </Button>
           </form>
         </Card>
       </div>
